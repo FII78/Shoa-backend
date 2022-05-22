6c808b36b2ccb6f97973a0390a3408ac43a476b6
Fitsum <fitsesile@gmail.com>
2022-05-22 23:38:41 +0300

    init validate middleware

---
+++ src/middlewares/validate.js
@@ -0,0 +1,8 @@
+const Joi = require('joi');
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+
+
+
+module.exports = validate;
