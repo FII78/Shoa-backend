733f7f065f20de743691b12e4f9e51b2db2a9ab4
Fitsum <fitsesile@gmail.com>
2022-05-22 23:39:17 +0300

    object and valid schema added for validate

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,6 +3,10 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 
-
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
+ 
+};
 
 module.exports = validate;
