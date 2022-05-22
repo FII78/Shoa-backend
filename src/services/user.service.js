539de3bf272e865609e10fd47dc4e4d3793256f4
Fitsum <fitsesile@gmail.com>
2022-05-23 00:08:12 +0300

    query users service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -14,3 +14,16 @@ const createUser = async (userBody) => {
   return User.create(userBody);
 };
 
+/**
+ * Query for users
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+ const queryUsers = async (filter, options) => {
+  const users = await User.paginate(filter, options);
+  return users;
+};
