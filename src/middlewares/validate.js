d40841d65ee0c939b56ee6416c92d8ec729281ba
Fitsum <fitsesile@gmail.com>
2022-05-22 23:40:02 +0300

    object error message

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -10,7 +10,12 @@ const validate = (schema) => (req, res, next) => {
     .prefs({ errors: { label: 'key' }, abortEarly: false })
     .validate(object);
 
- 
+  if (error) {
+    const errorMessage = error.details.map((details) => details.message).join(', ');
+    return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+  }
+  Object.assign(req, value);
+  return next();
 };
 
 module.exports = validate;
