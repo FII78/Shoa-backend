5b10ecbeafee7c901f7e0f38de79cd48d8eac1ed
Fitsum <fitsesile@gmail.com>
2022-05-22 23:35:47 +0300

    init swaggerdef

---
+++ src/docs/swaggerDef.js
@@ -0,0 +1,14 @@
+const { version } = require('../../package.json');
+const config = require('../config/config');
+
+const swaggerDef = {
+  openapi: '3.0.0',
+  info: {
+    title: 'ERP API documentation',
+    version 
+      
+  },
+  
+};
+
+module.exports = swaggerDef;
