d09ab5e492921ce6af600df31531aca5108b6cb2
Fitsum <fitsesile@gmail.com>
2022-05-22 23:39:42 +0300

    joi used for validate

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -6,6 +6,10 @@ const ApiError = require('../utils/ApiError');
 const validate = (schema) => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
   const object = pick(req, Object.keys(validSchema));
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: 'key' }, abortEarly: false })
+    .validate(object);
+
  
 };
 
