f240a7532998e886fd4f3f111f2d5fe001f35451
Fitsum <fitsesile@gmail.com>
2022-05-22 23:59:14 +0300

    refresh token service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -31,3 +31,23 @@ const logout = async (refreshToken) => {
   }
   await refreshTokenDoc.remove();
 };
+
+/**
+ * Refresh auth tokens
+ * @param {string} refreshToken
+ * @returns {Promise<Object>}
+ */
+ const refreshAuth = async (refreshToken) => {
+  try {
+    const refreshTokenDoc = await tokenService.verifyToken(refreshToken, tokenTypes.REFRESH);
+    const user = await userService.getUserById(refreshTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await refreshTokenDoc.remove();
+    return tokenService.generateAuthTokens(user);
+  } catch (error) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate');
+  }
+};
+
