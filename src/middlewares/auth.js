0ac6446ded3c83320b4d961451c4f0a7ec2f35d8
Fitsum <fitsesile@gmail.com>
2022-05-22 23:36:47 +0300

    init auth middle ware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,8 @@
+const passport = require('passport');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const { roleRights } = require('../config/roles');
+
+
+
+module.exports = auth;
