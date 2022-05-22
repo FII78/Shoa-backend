3ec106d0d4475159d2706bfc6b80a29779cb420f
Fitsum <fitsesile@gmail.com>
2022-05-22 23:58:46 +0300

    logout service in auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -19,4 +19,15 @@ const loginUserWithEmailAndPassword = async (email, password) => {
   return user;
 };
 
-
+/**
+ * Logout
+ * @param {string} refreshToken
+ * @returns {Promise}
+ */
+const logout = async (refreshToken) => {
+  const refreshTokenDoc = await Token.findOne({ token: refreshToken, type: tokenTypes.REFRESH, blacklisted: false });
+  if (!refreshTokenDoc) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Not found');
+  }
+  await refreshTokenDoc.remove();
+};
