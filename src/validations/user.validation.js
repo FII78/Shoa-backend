d247726a18fcce99b3f0bfea9563f70784891def
Fitsum <fitsesile@gmail.com>
2022-05-23 00:21:57 +0300

    get users val.

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -10,3 +10,13 @@ const createUser = {
   }),
 };
 
+const getUsers = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
