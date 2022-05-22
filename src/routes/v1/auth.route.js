fa9006acbe495e58f10d118016b0ded7602afe68
Fitsum <fitsesile@gmail.com>
2022-05-22 23:49:31 +0300

    route for refresh token

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -9,3 +9,4 @@ const router = express.Router();
 router.post('/register', validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
+router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
