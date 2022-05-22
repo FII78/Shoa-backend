3f06c68281a0ce719b9e6ad7e49b192e247fde17
Fitsum <fitsesile@gmail.com>
2022-05-22 23:34:02 +0300

    get user added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,3 +4,15 @@ const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
 
+const createUser = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  res.status(httpStatus.CREATED).send(user);
+});
+
+const getUsers = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['name', 'role']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await userService.queryUsers(filter, options);
+  res.send(result);
+});
+
