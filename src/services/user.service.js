73256bed6e5b82b5cd0fa7ce22fc59d3ad6cd50f
Fitsum <fitsesile@gmail.com>
2022-05-23 00:09:52 +0300

    delete by user id

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -65,3 +65,25 @@ const createUser = async (userBody) => {
   return user;
 };
 
+/**
+ * Delete user by id
+ * @param {ObjectId} userId
+ * @returns {Promise<User>}
+ */
+ const deleteUserById = async (userId) => {
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  await user.remove();
+  return user;
+};
+
+module.exports = {
+  createUser,
+  queryUsers,
+  getUserById,
+  getUserByEmail,
+  updateUserById,
+  deleteUserById,
+};
