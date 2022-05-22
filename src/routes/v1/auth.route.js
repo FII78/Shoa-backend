d32772a42809d60056d780bda4d9a9a39db642d6
Fitsum <fitsesile@gmail.com>
2022-05-22 23:48:46 +0300

    route for login

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -7,3 +7,4 @@ const auth = require('../../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', validate(authValidation.register), authController.register);
+router.post('/login', validate(authValidation.login), authController.login);
