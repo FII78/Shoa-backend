16b54f677d5a9fed49d3cee5c9de68b859bd710a
Fitsum <fitsesile@gmail.com>
2022-05-27 13:33:37 +0300

    afterAll added

--- tests/utils/setupTestDB.js
+++ tests/utils/setupTestDB.js
@@ -10,7 +10,10 @@ const setupTestDB = () => {
         await Promise.all(Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany()));
       });
   
-   
+    afterAll(async () => {
+    await mongoose.disconnect();
+    });
+
   };
 
 module.exports = setupTestDB;
