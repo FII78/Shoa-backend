f623f65376a56848294cd673af374b358cdd5458
Fitsum <fitsesile@gmail.com>
2022-05-27 18:04:13 +0300

    error convertor

--- tests/unit/middlewares/error.test.js
+++ tests/unit/middlewares/error.test.js
@@ -6,3 +6,19 @@ const ApiError = require('../../../src/utils/ApiError');
 const config = require('../../../src/config/config');
 const logger = require('../../../src/config/logger');
 
+describe('Error middlewares', () => {
+    describe('Error converter', () => {
+        test('should return the same ApiError object it was called with', () => {
+          const error = new ApiError(httpStatus.BAD_REQUEST, 'Any error');
+          const next = jest.fn();
+    
+          errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse(), next);
+    
+          expect(next).toHaveBeenCalledWith(error);
+        });
+    
+    
+    
+    })   
+    });
+    
