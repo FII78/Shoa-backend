dfc764875889c2d3cc0701b5c20da0d52340ffc7
Fitsum <fitsesile@gmail.com>
2022-05-27 18:33:11 +0300

    404 prod tested for doc

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -5,7 +5,11 @@ const config = require('../../src/config/config');
 
 describe('Auth routes', () => {
     describe('GET /v1/docs', () => {
-        
+        test('should return 404 when running in production', async () => {
+            config.env = 'production';
+            await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
+            config.env = process.env.NODE_ENV;
+          });
       });
   });
   
