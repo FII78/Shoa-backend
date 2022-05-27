f1d2e369f1409cef451f8b10494b0694f2eb1222
Fitsum <fitsesile@gmail.com>
2022-05-27 13:32:33 +0300

    beforeAll func added

--- tests/utils/setupTestDB.js
+++ tests/utils/setupTestDB.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const config = require('../../src/config/config');
 
-
+const setupTestDB = () => {
+    beforeAll(async () => {
+      await mongoose.connect(config.mongoose.url, config.mongoose.options);
+    });
+  
+   
+  };
 
 module.exports = setupTestDB;
