abee8ff779c517d71c9875326b6f314a9ac83c30
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-06-21 12:32:33 +0300

    fixed error

--- src/controllers/hr/employee.controller.js
+++ src/controllers/hr/employee.controller.js
@@ -3,9 +3,24 @@ const pick = require('../../utils/pick');
 const ApiError = require('../../utils/ApiError');
 const catchAsync = require('../../utils/catchAsync');
 const { employeeService } = require('../../services');
+const cloudinary = require('../../utils/cloudinary');
 
 const createEmployee = catchAsync(async (req, res) => {
-  const employee = await employeeService.createEmployee(req.body);
+  const result = await cloudinary.uploader.upload(req.file.path);
+
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'File not uploaded');
+  }
+
+  var userbody = {};
+  userbody = JSON.parse(req.body.body);
+
+  console.log('user body', userbody);
+
+  userbody.cv = result.secure_url;
+  userbody.cloudinary_id = result.public_id;
+
+  const employee = await employeeService.createEmployee(userbody);
   res.status(httpStatus.CREATED).send(employee);
 });
 
