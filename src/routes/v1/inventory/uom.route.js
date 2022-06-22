5be394babeac859445cff107cb30bab57385771c
Fitsum <fitsesile@gmail.com>
2022-06-22 20:10:24 +0300

    uom route

---
+++ src/routes/v1/inventory/uom.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const auth = require('../../../middlewares/auth');
+const validate = require('../../../middlewares/validate');
+const { uomValidation } = require('../../../validations');
+const { uomController } = require('../../../controllers');
+
+const router = express.Router();
+
+router.route('/')
+  .post(auth('manageUom'), validate(uomValidation.createUom), uomController.createUom)
+  .get(auth('manageUom'), validate(uomValidation.getUoms), uomController.getUoms);
+
+router
+  .route('/:uomId')
+  .get(auth('manageUom'), validate(uomValidation.getUom), uomController.getUom)
+  .patch(auth('manageUom'), validate(uomValidation.updateUom), uomController.updateUom);
+
+module.exports = router;
