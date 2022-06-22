677c5ab6e3d648e64bec6379894e41f3d37cb990
Fitsum <fitsesile@gmail.com>
2022-06-22 20:10:04 +0300

    item

---
+++ src/routes/v1/inventory/item.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const auth = require('../../../middlewares/auth');
+const validate = require('../../../middlewares/validate');
+const { itemValidation } = require('../../../validations');
+const { itemController } = require('../../../controllers');
+
+const router = express.Router();
+
+router.route('/')
+  .post(auth('manageItem'), validate(itemValidation.createItem), itemController.createItem)
+  .get(auth('manageItem'), validate(itemValidation.getItems), itemController.getItems);
+
+router
+  .route('/:itemId')
+  .get(auth('manageItem'), validate(itemValidation.getItem), itemController.getItem)
+  .patch(auth('manageItem'), validate(itemValidation.updateItem), itemController.updateItem);
+
+module.exports = router;
