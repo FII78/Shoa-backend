13897b3a99e42c2acab25ebb077fbbc15e357cd6
Fitsum <fitsesile@gmail.com>
2022-06-22 20:08:38 +0300

    idx models

--- src/models/index.js
+++ src/models/index.js
@@ -15,6 +15,12 @@ module.exports.LeaveRequest = require('./hr/leaveRequest.model');
 module.exports.TravelRequest = require('./hr/travelRequest.model');
 
 //inventory models
+module.exports.Item = require('./inventory/item.model');
+module.exports.Product = require('./inventory/product.model');
+module.exports.Grv = require('./inventory/grv.model');
+module.exports.Variant = require('./inventory/variant.model');
+module.exports.Uom = require('./inventory/uom.model');
+module.exports.Supplier = require('./inventory/supplier.model');
 
 //transport models
 module.exports.Vehicle = require('./vehicle.model');
